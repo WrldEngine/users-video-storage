3a020d266c4b04289061c76bc8e5fca4ddbd6ce5
WrldEngine <kamscienceit123.kamran@gmail.com>
2024-05-03 01:13:33 +0500

    admin_settings_extended

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,37 @@
 from django.contrib import admin
 from .models import Users, Videos, Comments
 
-admin.site.register(Users)
-admin.site.register(Videos)
-admin.site.register(Comments)
+@admin.register(Users)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "username",
+        "is_superuser",
+        "date_joined",
+        "verified",
+    ]
+
+    search_fields = ("username",)
+
+@admin.register(Videos)
+class VideosAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "title",
+        "video",
+        "date",
+    ]
+
+    search_fields = ("title",)
+
+@admin.register(Comments)
+class VideosAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "content",
+        "author",
+        "video_id",
+        "date",
+    ]
+
+    search_fields = ("author",)
